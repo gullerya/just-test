7098f3aa8e329973780a249764dcc699c3d2c412
Yuri Guller <gullerya@gmail.com>
2018-09-08 13:32:29 +0300

    renewing the framework layout, build process

--- build/tools/build-dist.js
+++ build/tools/build-dist.js
@@ -7,17 +7,17 @@ const
 	uglifyES = require('uglify-es');
 
 process.stdout.write('cleaning "dist"...');
-fsExtra.emptyDirSync('../dist');
+fsExtra.emptyDirSync('./dist');
 process.stdout.write('\t\t\t\x1B[32mOK\x1B[0m' + os.EOL);
 
 process.stdout.write('copying "src" to "dist"...');
-fsExtra.copySync('../src', '../dist');
+fsExtra.copySync('./src', './dist');
 process.stdout.write('\t\x1B[32mOK\x1B[0m' + os.EOL);
 
 process.stdout.write('minifying...');
-uglifyES.minify({'../dist/just-test.min.js': fs.readFileSync('../dist/just-test.js', {encoding: 'utf8'})});
+uglifyES.minify({'./dist/just-test.min.js': fs.readFileSync('./dist/just-test.js', {encoding: 'utf8'})});
 fs.writeFileSync(
-	'../dist/just-test.min.js',
-	uglifyES.minify(fs.readFileSync('../dist/just-test.js', {encoding: 'utf8'})).code
+	'./dist/just-test.min.js',
+	uglifyES.minify(fs.readFileSync('./dist/just-test.js', {encoding: 'utf8'})).code
 );
 process.stdout.write('\t\t\t\t\x1B[32mOK\x1B[0m' + os.EOL);
