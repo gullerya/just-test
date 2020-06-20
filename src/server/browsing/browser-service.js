894484b09a772bbaf0709ede242a60fca43b723a
gullerya <gullerya@gmail.com>
2020-06-20 17:08:23 +0300

    more changes on the go

--- src/server/browsing/browser-service.js
+++ src/server/browsing/browser-service.js
@@ -1,6 +1,10 @@
+import buildConfig from './browser-service-config.js';
+
+const
+	CONFIG_KEY = Symbol('config.key');
+
 export default class BrowserService {
 	constructor(config) {
 		this[CONFIG_KEY] = Object.freeze(buildConfig(config));
-
 	}
 }
