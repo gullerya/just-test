44fe06cfe448f9d7ffebc50f3da0c78443959e07
Yuri Guller <gullerya@gmail.com>
2020-09-26 15:20:08 +0300

    added environment service

--- src/environment/environment-service.js
+++ src/environment/environment-service.js
@@ -2,7 +2,13 @@ import Logger from '../logger/logger.js';
 
 const
 	logger = new Logger({ context: 'environment' }),
-	ENVIRONMENTS_KEY = Symbol('environments.key');
+	ENVIRONMENTS_KEY = Symbol('environments.key'),
+	ENVIRONMENT_BLUEPRINT = Object.freeze({
+		interactive: true,
+		browser: null,
+		device: null,
+		scheme: null
+	});
 
 export default class EnvironmentService {
 	constructor(configuration) {
@@ -16,19 +22,61 @@ export default class EnvironmentService {
 			const envArgs = configuration.cliArguments.envs.split(/,|;/);
 			for (const envArg of envArgs) {
 				logger.debug(`parsing '${envArg}'...`);
+				//	TODO
 			}
 		} else if (configuration.environments && configuration.environments.length) {
 			for (const env of configuration.environments) {
-				logger.debug(env);
+				envs.push(Object.assign(
+					{},
+					ENVIRONMENT_BLUEPRINT,
+					Object.entries(env)
+						.filter(([key]) => key in ENVIRONMENT_BLUEPRINT)
+						.reduce((pv, [key, value]) => pv[key] = value, {})
+				));
 			}
 		} else {
 			logger.info('no environment configuration specified, defaulting to interactive');
+			envs.push(Object.assign(
+				{},
+				ENVIRONMENT_BLUEPRINT,
+				{ interactive: true }
+			));
 		}
+		EnvironmentService.validateEnvironments(envs);
+		EnvironmentService.reduceIdenticalEnvironments(envs);
 
 		this[ENVIRONMENTS_KEY] = Object.freeze(envs);
 	}
 
 	get environments() {
 		return this[ENVIRONMENTS_KEY];
+	}
+
+	static validateEnvironments(envs) {
+		for (const env of envs) {
+			if (env.interactive && env.browser) {
+				throw new Error(`environment can NOT be interactive and define browser; violator: ${JSON.stringify(env)}`);
+			}
+			if (env.interactive && (env.device || env.scheme)) {
+				throw new Error(`interactive environment can NOT specify device or scheme; violator: ${JSON.stringify(env)}`);
+			}
+		}
+	}
+
+	static reduceIdenticalEnvironments(envs) {
+		const map = {};
+		const toBeRemoved = envs.filter(e => {
+			const hash = JSON.stringify(e);
+			if (hash in map) {
+				logger.info(`removing identical environment from the list (${hash})`);
+				return true;
+			} else {
+				map[hash] = true;
+				return false;
+			}
+		});
+		for (const tbr of toBeRemoved) {
+			envs.splice(envs.indexOf(tbr), 1);
+		}
 	}
 }
