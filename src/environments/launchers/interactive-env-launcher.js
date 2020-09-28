6ca2fc476227c5549e66817db4ba1de4cc48726c
Yuri Guller <gullerya@gmail.com>
2020-09-28 20:57:58 +0300

    wip;

--- src/environments/launchers/interactive-env-launcher.js
+++ src/environments/launchers/interactive-env-launcher.js
@@ -1,7 +1,19 @@
+/**
+ * 
+ * @param {Object} envConfig environment configuration
+ */
+import Logger from '../../logger/logger.js';
+import { obtainEffectiveConfig } from '../../configurer.js';
+
+const logger = new Logger({ context: 'interactive env launcher' });
+
 export default function launch(envConfig) {
 	if (!envConfig || !envConfig.interactive) {
 		throw new Error(`env configuration expected to have interactive set to true; got ${JSON.stringify(envConfig)}`);
 	}
-	//	TODO: this one should be able to be resolved by manual trigger
+
+	logger.info(`interactive enviroment is ready`);
+	logger.info(`to run tests open a browser and navigate <host>:${obtainEffectiveConfig('serverConfig').port}`);
+
 	return new Promise(() => { });
 }
