34e3fd9caf3ca497b7b591f834c8d755c6742c44
Yuri Guller <gullerya@gmail.com>
2021-05-10 09:28:21 +0300

    wip

--- tests/client/test-interop-utils.js
+++ tests/client/test-interop-utils.js
@@ -19,6 +19,41 @@ suite.test('getTestId - 3 parts', test => {
 suite.test('parseTestId - 2 parts', test => {
 	const base = getTestId('some', 'thing');
 	const [p1, p2] = parseTestId(base);
-	test.assertEqual('someq', p1);
+	test.assertEqual('some', p1);
 	test.assertEqual('thing', p2);
+});
+
+suite.test('getValidName - negative (undefined)', () => {
+	getValidName();
+}, {
+	expectError: 'name MUST be a string'
+});
+
+suite.test('getValidName - negative (null)', () => {
+	getValidName(null);
+}, {
+	expectError: 'name MUST be a string'
+});
+
+suite.test('getValidName - negative (number)', () => {
+	getValidName(5);
+}, {
+	expectError: 'name MUST be a string'
+});
+
+suite.test('getValidName - negative (empty string)', () => {
+	getValidName('');
+}, {
+	expectError: 'name MUST NOT be empty'
+});
+
+suite.test('getValidName - negative (emptish string)', () => {
+	getValidName('  \t ');
+}, {
+	expectError: 'name MUST NOT be empty'
+});
+
+suite.test('getValidName', test => {
+	const n = getValidName('  test ');
+	test.assertEqual('test', n);
 });
