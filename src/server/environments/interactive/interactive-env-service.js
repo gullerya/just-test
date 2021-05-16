554c030cc78e5b38d9d7381cb3992cdc4abfff50
Yuri Guller <gullerya@gmail.com>
2021-05-16 13:52:05 +0300

    fixing things a bit

--- src/server/environments/interactive/interactive-env-service.js
+++ src/server/environments/interactive/interactive-env-service.js
@@ -17,19 +17,30 @@ const logger = new Logger({ context: 'interactive env service' });
 class InteractiveEnvImpl extends EnvironmentBase {
 	constructor(sessionId, envConfig) {
 		super(sessionId);
-
 		this.envConfig = envConfig;
 	}
 
 	async launch() {
-		logger.info(`interactive session '${this.sessionId}' launched`);
+		logger.info(`interactive environment for session '${this.sessionId}' launched`);
 		logger.info(`open your browser and navigate to <host>:${serverConfig.port}`);
 		return Promise.resolve();
 	}
+
+	async dismiss() {
+		logger.info(`please close your browser/tab, environment '${this.envConfig.id}' is not longer alive`);
+	}
 }
 
-async function launch(sessionId) {
-	const result = new InteractiveEnvImpl(sessionId, serverConfig);
-	result.launch();
+/**
+ * 'launches' interactive browsing environment
+ * - won't actually do anything beside logging what use should do
+ * 
+ * @param {string} sessionId 
+ * @param {object} envConfig 
+ * @returns environment
+ */
+async function launch(sessionId, envConfig) {
+	const result = new InteractiveEnvImpl(sessionId, envConfig);
+	await result.launch();
 	return result;
 }
