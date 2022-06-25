728af99472ed3d98e6aef5bb45168fc64ed92be6
Yuri Guller <gullerya@gmail.com>
2022-06-25 11:46:03 +0300

    fixes

--- tests/coverage/test-coverage-utils.js
+++ tests/coverage/test-coverage-utils.js
@@ -1,11 +1,11 @@
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import { getSuite } from 'just-test/suite';
 import { buildJTFileCov } from '../../src/coverage/model/model-utils.js';
 
 const suite = getSuite('Coverage utils');
 
 suite.test('build model - negative (bad source URL, not a string)', async () => {
-	await expect(buildJTFileCov(5)).rejectedWith('source URL MUST be a non-empty string');
+	await assert.throws(async () => await buildJTFileCov(5)).rejectedWith('source URL MUST be a non-empty string');
 });
 
 suite.test('build model - negative (bad source URL, empty string)', async () => {
