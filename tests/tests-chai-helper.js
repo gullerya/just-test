7e8725cd61bc277d56629f1bb64cf762518dd64d
Yuri Guller <yuri.guller@vonage.com>
2023-11-22 21:10:12 +0200

    wuip;

--- tests/tests-chai-helper.js
+++ tests/tests-chai-helper.js
@@ -1,2 +1,3 @@
 import * as chai from 'chai';
 export const assert = globalThis.chai ? globalThis.chai.assert : chai.assert;
+export const expect = globalThis.chai ? globalThis.chai.expect : chai.expect;
