4e62bdb46799c7b59b63fcfe29e0abedc6b0f535
Yuri Guller <yuri.guller@vonage.com>
2023-11-22 21:25:04 +0200

    better way

--- tests/tests-chai-helper.js
+++ tests/tests-chai-helper.js
@@ -1,3 +1,10 @@
 import * as chai from 'chai';
 export const assert = globalThis.chai ? globalThis.chai.assert : chai.assert;
-export const expect = globalThis.chai ? globalThis.chai.expect : chai.expect;
+
+chai.util.addMethod(chai.assert, 'rejects', async (promise, error) => {
+    await promise
+        .then(() => assert.fail('expected promise to be rejected'))
+        .catch(e => {
+            assert.include(e.message, error);
+        });
+});
