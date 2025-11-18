3bba722cc056fb0f30ecb3c57d3f22f8c9bfca99
Yuri Guller <gullerya@gmail.com>
2025-11-18 09:08:57 +0200

    fix

--- src/common/error-utils.js
+++ src/common/error-utils.js
@@ -1,4 +1,8 @@
 export function processError(error) {
+    if (!(error instanceof Error)) {
+        return null;
+    }
+
     const cause = error.cause ? processError(error.cause) : undefined;
     const stacktrace = error.stack.split(/\r\n|\r|\n/)
         .map(l => l.trim())
